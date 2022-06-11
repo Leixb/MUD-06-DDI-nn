06154cb713f3e1d352e4ee325bdccb174c1087b5
LeixB <abone9999@gmail.com>
2022-06-11 17:52:25 +0200

    Set memory growth

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import os
 import random
 from contextlib import redirect_stdout
 
+import tensorflow as tf
 from tensorflow.keras.utils import set_random_seed
 from tensorflow.keras import regularizers, Input
 from tensorflow.keras.models import Model
@@ -73,7 +74,22 @@ def build_network(codes):
 # -- Usage:  train.py ../data/Train ../data/Devel  modelname
 # --
 
+def set_memory_growth():
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+      try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+          tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+      except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
 if __name__ == "__main__":
+
+    set_memory_growth()
 
     if len(sys.argv) != 4:
         print("Usage: train.py <trainfile> <validationfile>  <modelname>")
